7d756f6c86a26e9377de364440cc1d2b702b65d2
lucas.lima.pinheiro <lucas.lima.pinheiro@hotmail.com>
2017-03-02 11:18:34 -0500

    Lucas: Layout criado, adicionado paginas css, codigo enxuto

---
+++ Cabecalho.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Sistema de Gestão Escolar </title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/Layout_Style.css" rel="stylesheet">
+	<link rel="icon" type="image/png" href="_imagens/sge.png" size="16x16" maxlength="8" />
+</head>
+<body background="_imagens/10.jpg">
+  <nav class="navbar navbar-inverse" id="menu">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php">SGE</a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="Index.php">Home <span class="sr-only">(current)</span></a></li>
+						<li><a href="Matricula.php">Cadastrar Aluno</a></li>
+						<li><a href="GridAlunos.php">Listar Alunos</a></li>
+					</ul>
+				</div>
+			</div>
+    </nav>
+		<div id="interface">
---
+++ Rodape.php
@@ -0,0 +1,11 @@
+    <footer id="rodape">
+			<p>Rua das Quintas Nº148 - Goiana-PE Centro</br>
+				 E-mail: ultracomicstudio@gmail.com</br>
+				 Telefone: 081) 99420-4182<br />
+			<a href="https://www.facebook.com/StudioDeArteUltraComic/?fref=ts" target="_blank">Facebook</a> |
+			<a href="https://twitter.com/stultracomic" target="_blank"> Twitter </a> |
+			<a href="http://studiodearteultracomic.blogspot.com.br/" target="_blank"> Blog </a></p>
+		</footer>
+  </div>
+</body>
+</html>
